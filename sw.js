3cd4db8f0b50a14480fd8bfe2b5c96120ac41672
10neen <aladawyhassan2020@gmail.com>
2026-01-29 22:41:22 +0200

    Update sw.js
    تعديلات هامه للتاريخ

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ramadan-cache-v13"; // تحديث الإصدار لضمان تنشيط التعديلات الجديدة
+const CACHE_NAME = "ramadan-cache-v14"; // تحديث الإصدار لضمان تنشيط التعديلات الجديدة
 
 // قائمة الملفات الأساسية - تأكد أن الأسماء مطابقة تماماً للموجود في المجلد
 const urlsToCache = [
@@ -70,3 +70,4 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
